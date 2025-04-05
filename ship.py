bd1c0bd78537137ecd74d74c0b226b833e72589d
Daraab Malik <daraab.malik@student.manchester.ac.uk>
2025-04-05 17:07:18 +0100

    initial commit

--- ship.py
+++ ship.py
@@ -1,3 +1,16 @@
 class Ship():
-    def __init__():
-        pass
+    def __init__(self, oxygen, fuel):
+        self.oxygen = oxygen
+        self.fuel = fuel 
+
+    def add_fuel(self, fuel):
+        self.fuel += fuel
+
+    def add_oxygen(self, oxygen):
+        self.oxygen += oxygen
+
+    def ship_running(self, game_runnng):
+        if (game_runnng):
+            self.fuel -= 1
+            self.oxygen -= 1
+    
