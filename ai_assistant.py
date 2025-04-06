d0ce42bb0853d93d8a1a843af062050cf4c203c3
Noor Khalil <noor.khalil@student.manchester.ac.uk>
2025-04-06 01:32:28 +0100

    Adding features to shannon

--- ai_assistant.py
+++ ai_assistant.py
@@ -16,11 +16,9 @@ hunger = 3
 character_fuel = 10
 ship_fuel = 4
 
-if oxygen > 20:
-    level = "safe"
 status = [
             {"item": "oxygen", "safe_level": 20, "current_level": oxygen, "solution": "Plant more trees", "safety": "safe" if oxygen >= 20 else "not safe" }, 
-            {"item": "water", "safe_level": 20, "current_level": thirst, "solution": "Find ice blocks and bring them back to the spaceship", "safety": "safe" if thirst >= 20 else "not safe"},  
+            {"item": "thirst", "safe_level": 20, "current_level": thirst, "solution": "Find ice blocks and bring them back to the spaceship", "safety": "safe" if thirst >= 20 else "not safe"},  
             {"item": "hunger", "safe_level": 40, "current_level": hunger, "solution": "Plant more plants and harvest them", "safety": "safe" if hunger >= 40 else "not safe"},
             {"item": "character fuel", "safe_level": 10, "current_level": character_fuel, "solution": "Plant more plants and harvest them", "safety": "safe" if character_fuel >= 10 else "not safe"},
             {"item": "ship fuel", "safe_level": 10, "current_level": ship_fuel, "solution": "Plant more plants and harvest them", "safety": "safe" if ship_fuel >= 10 else "not safe"}
@@ -30,17 +28,35 @@ basic_potatoes = 2
 mars_potatoes = 2
 tree_potatoes = 1
 ice = 4
-coal = 4
-oil = 3
-inventory = [
-                {"item": "basic potatoes", "number": basic_potatoes, "solves": "hunger, oxygen (by replanting)"},
-                {"item": "mars potatoes", "number": mars_potatoes, "solves": "hunger (best option for food)"},
-                {"item": "tree potatoes", "number": tree_potatoes, "solves": "oxygen (best option for planting for oxygen)"},
-                {"item": "ice blocks", "number": ice, "solves": "thirst"},
-                {"item": "coal", "number": coal, "solves": "fuel"},
-                {"item": "ice blocks", "number": ice, "solves": "fuel"}
+rustalon = 3
+hexacron = 3
+xerocite = 3
+nytrazene = 3
+tatonium = 3
+aetherium = 3
+ores = [        
+            {"name": "rustalon", "number": rustalon},
+            {"name": "hexacron", "number": hexacron},
+            {"name": "xerocite", "number": xerocite}
+            ]
+
+plants = [
+            {"name": "basic potatoes", "number": basic_potatoes, "satiation": 1, "oxypot":1},
+            {"name": "mars potatoes", "number": mars_potatoes, "satiation": 3, "oxypot":0},
+            {"name": "tree potatoes", "number": tree_potatoes, "satiation": 0, "oxypot":3}
+            ]
+
+radioactive = [
+                    {"name": "nytrazene", "number": nytrazene},
+                    {"name": "tatonium", "number": tatonium},
+                    {"name": "aetherium", "number": aetherium}
+                    ]
+
+general = [
+                {"name": "ice", "number": ice}
                 ]
 
+inventory = [ores, plants, radioactive, general]
 
 async def good():
     client = Neuphonic(api_key=os.environ.get('NEUPHONIC_API_KEY'))
@@ -49,9 +65,10 @@ async def good():
         name='Shannon',
         prompt='''Check the statuses and inventory'''+ str(status)+ str(inventory)+ 
         '''and tell me about whichever item in status is at the lowest current_level first, then tell me about any other statuses when prompted.
-        If the level of an item is not safe, then you should check the through the inventory and find items which solve that problem.
-        and tell me to solve the problem by checking the inventory for that item and the related problem the item can solve and telling me that I should use it and how much of it there is . 
-        If there is no item in the inventory which corresponds to the danger item, tell me that I am low in inventory and tell me what the solution is.
+        For low hunger and oxygen levels: check through plants. Find which ones you have that have the highest oxypot or satiation depending on if you need the plant for oxygen or for food. 
+        If there is none that satisfies the need fetch the solution from the status.
+        To make a mars or tree potato we need 20 basic potatoes and 15 tatonium.
+        When prompted, state if we are able to make one by fetching that information from radioactive and plants, and if not what we have left that we need.
         All levels are percentages, so treat them as such. When prompted for more information about another item provide all the details you have. Dont wait for prompts''',
         greeting='Would you like me to tell you your next course of action?'
     ).data['agent_id']
@@ -122,4 +139,4 @@ async def evil():
     except KeyboardInterrupt:
         await agent.stop()
 
-asyncio.run(evil())
+asyncio.run(good())
